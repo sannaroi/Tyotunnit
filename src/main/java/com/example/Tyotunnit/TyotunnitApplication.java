619ed48fc335ddb1387d8b076b461cb7a45ca36e
Juslin Jukka <jukka.juslin@haaga-helia.fi>
2023-11-02 10:20:45 +0200

    vaihdettiin tietokanta h2:sta mysql:aan joka on arvosanan 5 esiehto

--- src/main/java/com/example/Tyotunnit/TyotunnitApplication.java
+++ src/main/java/com/example/Tyotunnit/TyotunnitApplication.java
@@ -45,8 +45,8 @@ public class TyotunnitApplication {
         	
         	AppUser user1 = new AppUser("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
 			AppUser user2 = new AppUser("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
-			urepository.save(user1);
-			urepository.save(user2);
+			//urepository.save(user1);
+			//urepository.save(user2);
 	
             log.info("fetch all workers");
 			for (Worker worker : repository.findAll()) {
